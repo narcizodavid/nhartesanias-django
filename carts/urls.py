1893baaa7aaa3bc7548d42aa9ddc2c45add9fa28
Narcizo Hinojosa <narcizohinojosa@gmail.com>
2022-01-16 21:27:09 -0500

    cuarto commit cart checkout

--- carts/urls.py
+++ carts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('minus_cart/<int:product_id>/<int:cart_item_id>/', views.minus_cart, name='minus_cart'),
     path('remove_cart/<int:product_id>/<int:cart_item_id>/', views.remove_cart, name='remove_cart'),
 
-    #path('checkout/', views.checkout, name='checkout'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
